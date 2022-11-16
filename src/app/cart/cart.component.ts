c3cd03d2e8791cf417999eca950b0322f9118043
hnupanb <h.nupanb@gmail.com>
2022-11-16 16:08:23 -0500

    3 files modified, 3 added

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+export class CartComponent  {
+  items= this.cartService.getItems();
+  constructor (private cartService:CartService){ }
+
+}
